fb5f39938ec2d8ed4fefec14151e29e34193a9ba
akitl <akitl1@yahoo.fr>
2021-01-06 15:29:39 +0100

    first commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1 : IParler
+    {
+
+        public Class1(string name)
+        {
+            this.name = name;
+        }
+
+        public Class1(Class1 c)
+        {
+            name = c.name;
+            i = c.i;
+        }
+
+        public int i {get;set;}
+
+        private string name;
+        public string Name { set { name = value; } get { return name; } }
+
+        public void Cris()
+        {
+            Console.WriteLine("yaah");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1("test");
+
+            Console.WriteLine(a.Name);
+            a.i = 2;
+            a.Name = "ok";
+            F(a);
+            Console.WriteLine(a.Name);
+
+            a.Cris();
+
+            int i = 0;
+            S(ref i);
+            Console.WriteLine(i);
+
+            Console.ReadLine();
+
+        }
+
+        public static void F(Class1 c) 
+        {
+            Class1 cc = new Class1(c);
+            cc.Name = "non";
+            c.Name = "non";
+        }
+
+        public static void S(ref int i)
+        {
+            i = 5;
+        }
+
+    }
+}
